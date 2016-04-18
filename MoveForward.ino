45cd60fdbe48851cafcfa7955048a4d36d4c6469
Peter Peng <dr.peter2028@gmail.com>
2016-04-17 23:55:02 -0400

    Create MoveForward.ino

---
+++ MoveForward.ino
@@ -0,0 +1,13 @@
+/** MoveForwad
+* This program tells the robot to move forward
+* Later, we will add code to make it follow a straight line
+* Then we will have it capable of going through multiple corrdinates
+*/
+void setup(){
+
+}
+
+void loop(){
+
+}
+
