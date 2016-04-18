09b6ed20ae64f2e1227a00a3a6ee92bc9721d78d
Peter Peng <dr.peter2028@gmail.com>
2016-04-18 00:52:00 -0400

    Update and rename MoveForward.ino to PathFollowing.ino
    Created new functions for position control

--- PathFollowing.ino
+++ PathFollowing.ino
@@ -19,6 +19,9 @@ int in2 = 7;
 int in3 = 8;
 int in4 = 9;
 int enb = 10; //pwm
+float x_ref = 0.5, y_ref = 0.5; //define a rectangular path
+float x0, y0; //initial x0,y0;
+int pathNum = 0;
 
 void setup() {
   Serial.begin(9600);
@@ -29,12 +32,30 @@ void setup() {
   pinMode(enb, OUTPUT);
   pinMode(in3, OUTPUT);
   pinMode(in4, OUTPUT);
+  rfRead();
+  x0 = marker.x;
+  y0 = marker.y
 }
 
 void loop() {
-motorStraight();
+//motorStraight();
+  switch(pathNum){
+    case 0: break;
+    case 1: break;
+    case 2: break;
+    case 3: break;
+  }
 }
 
+//move in the direction of x-axis following path of constant y value
+void moveAlongX(float terminationX, float referenceY){
+  
+}
+
+//move in the direction of y-axis following path of constant x value
+void moveAlongY(float terminationY, float referenceX){
+  
+}
 
 void motorStraight(){
   digitalWrite(in1,LOW);
@@ -43,6 +64,14 @@ void motorStraight(){
   digitalWrite(in3,HIGH);
   digitalWrite(in4,LOW);
   analogWrite(enb,255);
+}
+
+void motorForward(int delta_PWM){
+  if(delta_PWM <= 0){
+    
+  } else {
+    
+  }
 }
 
 void rfSetup() {
